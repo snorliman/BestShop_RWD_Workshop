b4678e0f5aebfe71c8521764fc01eb60295504be
snorliman <snorli@o2.pl>
2020-08-04 18:24:28 +0200

    skończony kalkulator

--- main.js
+++ main.js
@@ -3,18 +3,19 @@ const products = document.querySelector('#products');
 const orders =document.querySelector('#orders');
 const accounting =document.querySelector('#accounting');
 const terminal =document.querySelector('#terminal');
-const calc_sumarry =document.querySelector('.calc__summary');
-const summaryProduct =calc_sumarry.querySelector('[data-id="products"]');
-const summaryOrders =calc_sumarry.querySelector('[data-id="orders"]');
-const summaryPackage =calc_sumarry.querySelector('[data-id="package"]');
+const calcSummary =document.querySelector('.calc__summary');
+const summaryProduct =calcSummary.querySelector('[data-id="products"]');
+const summaryOrders =calcSummary.querySelector('[data-id="orders"]');
+const summaryPackage =calcSummary.querySelector('[data-id="package"]');
+const summaryAccount =calcSummary.querySelector('[data-id="accounting"]');
+const summaryTerminal =calcSummary.querySelector('[data-id="terminal"]');
 
-const select_dropdown =document.querySelector('.calc__select');
-const basic = select_dropdown.querySelector('[data-value="basic"]');
-const professional = select_dropdown.querySelector('[data-value="professional"]');
-const premium = select_dropdown.querySelector('[data-value="premium"]');
-const summary_total =document.querySelector('.summary__total');
-const price = document.querySelector('.total__price');
-const select_input = document.querySelector('.select__input');
+const selectDropdown =document.querySelector('.calc__select');
+const basic = selectDropdown.querySelector('[data-value="basic"]');
+const professional = selectDropdown.querySelector('[data-value="professional"]');
+const premium = selectDropdown.querySelector('[data-value="premium"]');
+const summaryTotal =document.querySelector('.summary__total');
+const selectInput = document.querySelector('.select__input');
 const total_Price = document.querySelector('.total__price');
 
 let total = 0;
@@ -34,30 +35,35 @@ const data  = {
     terminal: 5
 }
 function totalPrice () {
-    const show = summary_total.querySelectorAll(".open").length > 0;
+    const show = calcSummary.querySelectorAll(".open").length > 0;
     if (show) {
-        price.classList.add("open");
+        summaryTotal.classList.add("open");
     } else {
-        price.classList.remove("open");
+        summaryTotal.classList.remove("open");
     }
 
     if (accounting.checked) {
-        accountValue += data.accounting;
+        accountValue = data.accounting;
+    } else {
+        accountValue = 0;
     }
     if (terminal.checked) {
-        terminalValue += data.terminal;
+        terminalValue = data.terminal;
+    } else {
+        terminalValue = 0;
     }
-    if (select_input.value === "Basic") {
-        packageValue += data.package.basic;
+    if (selectInput.value === "Basic") {
+        packageValue = data.package.basic;
+    } else if (selectInput.value === "Professional") {
+        packageValue = data.package.professional;
+    }else if (selectInput.value === "Premium") {
+        packageValue = data.package.premium;
+    } else {
+        packageValue = 0;
     }
-    if (select_input.value === "Professional") {
-        packageValue += data.package.professional;
-    }
-    if (select_input.value === "Premium") {
-        packageValue += data.package.premium;
-    }
-    total += (products.value * 0.5) + (orders.value * 0.25) + accountValue + terminalValue + packageValue;
+    total = (products.value * data.products) + (orders.value * data.orders) + accountValue + terminalValue + packageValue;
     total_Price.innerText = "$ " + total;
+    return total;
 }
 
 function productsInput() {
@@ -89,36 +95,32 @@ orders.addEventListener('keyup', ordersInput);
 orders.addEventListener('change',ordersInput);
 
 accounting.addEventListener('click',function () {
-    const account = calc_sumarry.querySelector('[data-id="accounting"]');
     if(this.checked) {
-
-        account.classList.add('open');
-        account.querySelector('.item__price').innerText = "$ " + data.accounting;
+        summaryAccount.classList.add('open');
+        summaryAccount.querySelector('.item__price').innerText = "$ " + data.accounting;
     }
     else {
-        account.classList.remove('open');
+        summaryAccount.classList.remove('open');
     }
     totalPrice();
 });
 terminal.addEventListener('click',function () {
-    const term = calc_sumarry.querySelector('[data-id="terminal"]');
     if(this.checked) {
-
-        term.classList.add('open');
-        term.querySelector('.item__price').innerText = "$ " + data.terminal;
+         summaryTerminal.classList.add('open');
+        summaryTerminal.querySelector('.item__price').innerText = "$ " + data.terminal;
     }
     else {
-        term.classList.remove('open');
+        summaryTerminal.classList.remove('open');
     }
     totalPrice();
 });
 
-select_dropdown.addEventListener('click', function() {
-    select_dropdown.classList.toggle('open'); });
+selectDropdown.addEventListener('click', function() {
+    selectDropdown.classList.toggle('open'); });
 
 basic.addEventListener('click', function(){
-    select_input.value = "Basic";
-    if(select_input.value === "Basic") {
+    selectInput.value = "Basic";
+    if(selectInput.value === "Basic") {
        summaryPackage.classList.add('open');
         summaryPackage.querySelector('.item__calc').innerText = "Basic";
        summaryPackage.querySelector('.item__price').innerText = '$ ' + data.package.basic ;
@@ -126,8 +128,8 @@ basic.addEventListener('click', function(){
     totalPrice();
 });
 professional.addEventListener('click', function(){
-    select_input.value = "Professional";
-    if(select_input.value === "Professional") {
+    selectInput.value = "Professional";
+    if(selectInput.value === "Professional") {
         summaryPackage.classList.add('open');
         summaryPackage.querySelector('.item__calc').innerText = "Professional";
         summaryPackage.querySelector('.item__price').innerText = '$ ' + data.package.professional ;
@@ -135,8 +137,8 @@ professional.addEventListener('click', function(){
     totalPrice();
 });
 premium.addEventListener('click', function(){
-    select_input.value = "Premium";
-    if(select_input.value === "Premium") {
+    selectInput.value = "Premium";
+    if(selectInput.value === "Premium") {
         summaryPackage.classList.add('open');
         summaryPackage.querySelector('.item__calc').innerText = "Premium";
         summaryPackage.querySelector('.item__price').innerText = '$ ' + data.package.premium ;
